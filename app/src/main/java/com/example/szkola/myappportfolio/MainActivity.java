0d4975b5a74adb8caa731b3643ae613c59a1d861
Buli1212 <bulisnie@hotmail.com>
2015-08-18 00:26:35 +0100

    adressing feedback

--- app/src/main/java/com/example/szkola/myappportfolio/MainActivity.java
+++ app/src/main/java/com/example/szkola/myappportfolio/MainActivity.java
@@ -1,6 +1,6 @@
 package com.example.szkola.myappportfolio;
 
-import android.content.DialogInterface;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,61 +11,69 @@ import android.widget.Toast;
 
 public class MainActivity extends ActionBarActivity {
 
-
-
-
-
+    private Toast mAppToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    /**no settings menu used at this point*/
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+    public void showToast(View v){
+        //Stop any previous toasts
+        if(mAppToast !=null){
+            mAppToast.cancel();
         }
 
-        return super.onOptionsItemSelected(item);
+        mAppToast = Toast.makeText(this,"Clicking this button will lunch "+((Button)v).getText().toString(),Toast.LENGTH_SHORT);
+        mAppToast.show();
     }
 
+
     public void lunchApps(View v){
+
         switch(v.getId()){
             case R.id.btnSpStreamer:
-                Toast.makeText(getApplicationContext(), "Clicking this button will lunch the Spotify Streamer", Toast.LENGTH_SHORT).show();
+                showToast(v);
                 break;
             case R.id.btnScores:
-                Toast.makeText(getApplicationContext(), "Clicking this button will lunch the Scores App", Toast.LENGTH_SHORT).show();
+                showToast(v);
                 break;
             case R.id.btnLibrary:
-                Toast.makeText(getApplicationContext(), "Clicking this button will lunch the Library App", Toast.LENGTH_SHORT).show();
+                showToast(v);
                 break;
             case R.id.btnBuildItBigger:
-                Toast.makeText(getApplicationContext(), "Clicking this button will lunch the Build It Bigger App", Toast.LENGTH_SHORT).show();
+                showToast(v);
                 break;
             case R.id.btnXYZ:
-                Toast.makeText(getApplicationContext(), "Clicking this button will lunch the XYZ Reader App", Toast.LENGTH_SHORT).show();
+                showToast(v);
                 break;
             case R.id.btnCapstone:
-                Toast.makeText(getApplicationContext(), "Clicking this button will lunch my own app", Toast.LENGTH_SHORT).show();
+                showToast(v);
                 break;
         }
     }
